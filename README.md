b62a32c2f8aef74e02fb983f54b93dc7ab184f3b
mitraksh <rathodmitraksh@gmail.com>
2022-11-11 14:23:36 +0530

    update files

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # <img src="./images/Octocat.png" width="100" height="100">
 
 ## github repo4 Introduction 
-[repo4 link](https://github.com/mitraksh/repo4)
+[repo4 link](https://github.com/mitraksh/repo4.git)
 #### repo4 displays the repo2 & repo3's versions from repo1. Updates the versions of repo2 /7 repo3's versions from `npm i @mitraksh/repo2 --no-save`
 ### How to run this repo?
 
-#### To run this repo from node itself use `node indexrepo4.js` and to run it via Docker use `docker build -t getting-started .` To check the ENVIRONMENT variables use ` docker compose --env-file config.env config`. To change the environment variables go to `repo4 > config.env > NODE_ENV=production/development`.
+#### To run this repo from node itself use `npm install` and to run it via Docker use `docker build -t getting-started .` To check the ENVIRONMENT variables use ` docker compose --env-file config.env config`. To change the environment variables go to `repo4 > config.env > NODE_ENV=production/development`. Finally run `docker run getting-started`.
--- indexrepo4.js
+++ indexrepo4.js
@@ -24,66 +24,19 @@ var file_found = false;
 dotenv.config({ path: './config.env' });
 console.log('Environment'+process.env.NODE_ENV);
 
-fs.readdir(newpath, function (err, files) {
-    //handling error
-    if (err) {
-        return console.log('Unable to scan directory: ' + err);
-    }else if(!err){
-    //listing all files using forEach
-    files.forEach(function (file) {
-        // Do whatever you want to do  with the file
-        if(file == "repo1"){
-            file_found = true;
-            console.log("File found.");
-        }else if(file != "repo1"){
-          //file_found = false;
-            console.log("File Not Found.", file);
-        }
-    });
-
-    console.log(file_found+' = file found')
-
-    if(file_found == false){
-    console.log('Installing files...\nPlease wait.');
-             try {
-                exec(
-                  `cd ${newpath}  && mkdir repo1 && cd ${newpath+'repo1/'} && git clone https://github.com/mitraksh/scopedRepo1.git repo1`,
-                  (err, stdout, stderr) => {
-                    if (err) {
-                      console.error(err);
-                    }
-                    
-                    
-                    else if(!err) {
-                      console.log(`output: ${stdout}`);
-                      console.log(`message: ${stderr}`);
-                    }
-                  }
-                );
-              } catch (error) {
-                throw new Error(error);
-              }
-    }else if(file_found == true){
-      const repo1 = require('../repo1/myobject.json');
-      const repo2_version = repo1.repo2;
-      const repo3_version = repo1.repo3;
-      console.log(repo3_version+' repo2 v');
-      try {
-        exec(
-          `cd ${dir} && npm i @mitraksh/repo2@${repo2_version}--no-save && npm i @mitraksh/repo3@${repo3_version} --no-save`,
-          (err, stdout, stderr) => {
-            if (err) {
-              console.error(err);
-            }
-            else if(!err) {
-              console.log(`output: ${stdout}`);
-              console.log(`message: ${stderr}`);
-            }
-          }
-        );
-      } catch (error) {
-        throw new Error(error);
+try {
+  exec(
+    `cd ${dir} && npm i @mitraksh/repo2@${repo2_version}--no-save && npm i @mitraksh/repo3@${repo3_version} --no-save`,
+    (err, stdout, stderr) => {
+      if (err) {
+        console.error(err);
+      }
+      else if(!err) {
+        console.log(`output: ${stdout}`);
+        console.log(`message: ${stderr}`);
       }
     }
+  );
+} catch (error) {
+  throw new Error(error);
 }
-});
